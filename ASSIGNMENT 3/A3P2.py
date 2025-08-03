306c4fd34f31b177366bf29c399f910afb3b9164
karfa113 <karfamonojit3117@gmail.com>
2025-08-03 19:10:12 +0530

    modified code

--- ASSIGNMENT 3/A3P2.py
+++ ASSIGNMENT 3/A3P2.py
@@ -1,13 +1,12 @@
-user = input("Enter a string: ")
-user = list(user)
+def count(s):
+    upper = lower = 0
+    for i in user:
+        if i.isupper():
+            upper += 1
+        elif i.islower():
+            lower += 1
+    print(f"Uppercase characters: {upper}")
+    print(f"Lowercase characters: {lower}")
 
-upper = 0
-lower = 0
-for i in user:
-    if i.isupper():
-        upper += 1
-    elif i.islower():
-        lower += 1
-
-print(f"Uppercase characters: {upper}")
-print(f"Lowercase characters: {lower}")
+user = list(input("Enter a string: "))
+count(user)
