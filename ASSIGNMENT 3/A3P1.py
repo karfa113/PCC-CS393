1398b3ff7fc9943bf14f29208b86dc0a6554891c
karfa113 <karfamonojit3117@gmail.com>
2025-08-03 18:58:24 +0530

    modified code

--- ASSIGNMENT 3/A3P1.py
+++ ASSIGNMENT 3/A3P1.py
@@ -1,16 +1,12 @@
-word = input("Enter a string without any space: ")
-word = list(word.lower())
-i = 0
+def reduce(s):
+    stack = list()
+    for i in s:
+        if stack and stack[-1] == i:
+            stack.pop()
+        else:
+            stack.append(i)
+    return "".join(stack) if stack else "Empty string"
 
-while i < len(word) - 1:
-    if word[i] == word[i+1]:
-        del word[i:i+2]
-        i = 0
-    else:
-        i += 1
-
-if word:
-    for i in word:
-        print(i, end="")
-else:
-    print("Empty String")
+sample = input("Enter a string: ")
+print(f"Input - {sample}")
+print(f"Output - {reduce(sample)}")
